3c07d85c6146b0a6b322d4cfb9320888ef486e29
AYUSH RANJAN <63737303+aayusranjan@users.noreply.github.com>
2022-04-16 23:08:03 +0530

    Update forecast.js

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,4 +1,40 @@
-// interecting with an api
+/* alternet optioin by usin class
+*/
+class Forecast{
+    constructor(){
+        this.key = "dCGAN4hpaEA9dbfiZAGb9bi7p7Le8DWv";
+        this.weatherURI =
+          "http://dataservice.accuweather.com/currentconditions/v1/";
+        this.cityURI =
+          "http://dataservice.accuweather.com/locations/v1/cities/search";
+    }
+    async updateCity(city){
+     const  cityDets = await getCity(city);
+     const weather = await this.getWeather(cityDets.key);
+     return {
+         cityDets, weather
+     };
+
+    }
+    async getCity(city){
+        const query = `?apikey=${this.key}&q=${city}`;
+
+        const response = await fetch (this.cityURI + query);
+        const data = await response.json();
+
+        return data[0];
+    }
+    async getWeather(id){
+        const query = `${id}?apikey=${this.key}`;
+        const response = await fetch(this.weatherURI + query);
+        const data = await response.json();
+        return data[0];
+    }
+}
+
+
+/*
+ // interecting with an api
 const key = "kPmMxbY0vlWcxS0K3BYv7wbvbAxaNIxW";
 // get weather information
 
@@ -31,3 +67,5 @@ const getCity = async (city) => {
 //     console.log(data);
 // })
 // .catch(err => console.log(err));
+
+*/
